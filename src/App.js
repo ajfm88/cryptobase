4c546b5f69ed69cd10772f605181cb5787971613
unknown <ajfoucaultmo@mail.usf.edu>
2022-10-25 13:15:41 -0700

    06. Coin Search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import { ThemeProvider } from './context/ThemeContext';
+import Home from './routes/Home';
+import Signin from './routes/Signin';
+import Signup from './routes/Signup';
+import Account from './routes/Account';
+import axios from 'axios';
+
 function App() {
-  return <ThemeProvider></ThemeProvider>;
+  const [coins, setCoins] = useState([]);
+
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home coins={coins} />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CoinSearch = ({}) => {
+  console.log('hi');
+};
+
+export default CoinSearch;
