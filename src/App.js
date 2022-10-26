68a2440937d1e4139486ff2377e89c76890c8eb9
unknown <ajfoucaultmo@mail.usf.edu>
2022-10-26 12:42:34 -0700

    07. API - Coin Gecko

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { ThemeProvider } from './context/ThemeContext';
@@ -10,6 +10,16 @@ import axios from 'axios';
 
 function App() {
   const [coins, setCoins] = useState([]);
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      // console.log(response.data)
+    });
+  }, [url]);
 
   return (
     <ThemeProvider>
